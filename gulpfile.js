906e4647e4eedda49741df1162b9faefec8fe665
Elving Rodriguez <elving.pr@gmail.com>
2014-02-14 21:48:04 -0400

    Concat test files to serve only one test file to mocha-phantomjs.

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,17 @@
-var exec = require('child_process').exec;
-var gulp = require('gulp');
+var exec = require('child_process').exec,
 
-gulp.task('test', function() {
+    gulp = require('gulp'),
+    size = require('gulp-size'),
+    concat = require('gulp-concat')
+
+gulp.task('build_tests', function() {
+  return gulp.src('./test/**/!(tests)**.js')
+    .pipe(concat('tests.js'))
+    .pipe(size())
+    .pipe(gulp.dest('./test'))
+});
+
+gulp.task('test', ['build_tests'], function() {
   var path = './node_modules/mocha-phantomjs/bin/mocha-phantomjs';
   var command = '/test/index.html';
 
