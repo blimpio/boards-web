5fc67c77f19be6ae68afc5869385a8f1b95f7056
Elving Rodriguez <elving.pr@gmail.com>
2014-02-20 13:40:13 -0400

    Add accounts drop down view.

---
+++ app/views/accounts-dropdown.js
@@ -0,0 +1,26 @@
+module.exports = Zeppelin.View.extend({
+  el: 'div.accounts-dropdown',
+
+  name: 'AccountsDropdown',
+
+  template: require('templates/accounts-dropdown'),
+
+  collection: _.getCollection('Accounts'),
+
+  events: {
+    'click div.accounts-dropdown__current': 'toggle'
+  },
+
+  elements: {
+    'accountsList': 'ol.accounts-dropdown__list'
+  },
+
+  context: function() {
+    return this.collection.getPresenters(['accounts', 'currentAccount']);
+  },
+
+  toggle: function() {
+    this.$accountsList.toggle();
+    return this;
+  }
+});
---
+++ test/views/accounts-dropdown-test.js
@@ -0,0 +1,77 @@
+describe('AccountsDropdown', function() {
+  var AccountsDropdown = require('views/accounts-dropdown');
+
+  before(function() {
+    _.getCollection('Accounts').reset([{
+      id: 1,
+      name: 'ACME Inc',
+      slug: 'acme-inc',
+      image_url: ''
+    }, {
+      id: 4,
+      name: 'Blimp LLC',
+      slug: 'blimp',
+      image_url: ''
+    }]);
+  });
+
+  beforeEach(function() {
+    $('#application').html('<div class="accounts-dropdown"></div>');
+
+    this.AccountsDropdown = new AccountsDropdown({
+      collection: _.getCollection('Accounts')
+    });
+
+    this.AccountsDropdown.render();
+  });
+
+  afterEach(function() {
+    this.AccountsDropdown.remove();
+    delete this.AccountsDropdown;
+  });
+
+  after(function() {
+    _.getCollection('Accounts').reset();
+  });
+
+  it('should exist.', function() {
+    expect(this.AccountsDropdown).to.exist;
+  });
+
+  it('should have a name property.', function() {
+    expect(this.AccountsDropdown.name).to.exist;
+    expect(this.AccountsDropdown.name).to.equal('AccountsDropdown');
+  });
+
+  it('should have a template property.', function() {
+    expect(this.AccountsDropdown.template).to.exist;
+  });
+
+  it('should have a collection property.', function() {
+    expect(this.AccountsDropdown.collection).to.exist;
+    expect(this.AccountsDropdown.collection.name).to.equal('Accounts');
+  });
+
+  it('should have an events property.', function() {
+    expect(this.AccountsDropdown.events).to.exist;
+  });
+
+  it('should have an elements property.', function() {
+    expect(this.AccountsDropdown.elements).to.exist;
+  });
+
+  describe('context', function() {
+    it('should return the template context.', function() {
+      var context = this.AccountsDropdown.context();
+      expect(context).to.exist;
+      expect(_.keys(context)).to.eql(['accounts', 'currentAccount']);
+    });
+  });
+
+  describe('toggle', function() {
+    it('should toggle the dropdown element.', function() {
+      this.AccountsDropdown.toggle();
+      expect(this.AccountsDropdown.$('ol.accounts-dropdown__list').is(':visible')).to.be.true;
+    });
+  });
+});
