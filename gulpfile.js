1ac42d08db9b670979ca21b6914e5d9b38fbc6d5
Elving Rodriguez <elving.pr@gmail.com>
2014-03-03 12:12:19 -0400

    Fix file paths for gulp deploy task.

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,32 @@
-var s3 = require('gulp-s3'),
+var Q = require('q'),
+    s3 = require('gulp-s3'),
     git = require('git-rev'),
     gulp = require('gulp'),
     karma = require('gulp-karma'),
-    brunch = require('brunch');
+    brunch = require('brunch'),
+    coffee = require('coffee-script'),
+    imagemin = require('gulp-imagemin'),
 
-gulp.task('build:dev', function(cb) {
+    aws = require('./aws-config');
+
+gulp.task('build:dev', function() {
+  var deferred = Q.defer();
+
   brunch.build({}, function() {
-    cb();
+    deferred.resolve();
   });
+
+  return deferred.promise;
 });
 
-gulp.task('build:prod', function(cb) {
+gulp.task('build:prod', function() {
+  var deferred = Q.defer();
+
   brunch.build({production: true}, function() {
-    cb();
+    deferred.resolve();
   });
+
+  return deferred.promise;
 });
 
 gulp.task('test:dev', ['build:dev'], function() {
@@ -41,21 +54,54 @@ gulp.task('test:prod', ['build:prod'], function() {
   }));
 });
 
-gulp.task('deploy', ['build:prod'], function(cb) {
-  var aws = require('./aws-config'),
-      options = {};
+gulp.task('deploy:js', function() {
+  var deferred = Q.defer();
 
   git.short(function (hash) {
-    options.uploadPath = '/' + hash + '/';
-
     gulp.src([
-      'public/js/*',
-      'public/css/*',
-      'public/images/*'
+      'public/js/*'
     ], {read: false})
-    .pipe(s3(aws, options))
-    .on('end', function() {
-      cb();
+    .pipe(s3(aws, {uploadPath: '/' + hash + '/js/'}))
+    .on('close', function() {
+      deferred.resolve();
     });
   });
+
+  return deferred.promise;
 });
+
+
+gulp.task('deploy:css', function() {
+  var deferred = Q.defer();
+
+  git.short(function (hash) {
+    gulp.src([
+      'public/css/*'
+    ], {read: false})
+    .pipe(s3(aws, {uploadPath: '/' + hash + '/css/'}))
+    .on('close', function() {
+      deferred.resolve();
+    });
+  });
+
+  return deferred.promise;
+});
+
+gulp.task('deploy:images', function() {
+  var deferred = Q.defer();
+
+  git.short(function (hash) {
+    gulp.src([
+      'public/images/*'
+    ], {read: false})
+    .pipe(imagemin())
+    .pipe(s3(aws, {uploadPath: '/' + hash + '/images/'}))
+    .on('close', function() {
+      deferred.resolve();
+    });
+  });
+
+  return deferred.promise;
+});
+
+gulp.task('deploy', ['build:prod', 'deploy:js', 'deploy:css', 'deploy:images']);
