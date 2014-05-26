de9b76ccee16cb43317b18328a81f65830d84bba
Elving Rodriguez <elving.pr@gmail.com>
2014-05-26 16:24:30 -0400

    Fix notification card previews.

--- app/activity/views/notification.js
+++ app/activity/views/notification.js
@@ -10,8 +10,44 @@ module.exports = Zeppelin.ModelView.extend({
     return template(context);
   },
 
+  elements: {
+    card: 'span.notification-preview-card'
+  },
+
   context: function() {
     return this.model.getData();
+  },
+
+  onRender: function() {
+    if (this.model.getType() === 'card:created') {
+      this.renderCardPreview(this.model.getData().card);
+    }
+  },
+
+  renderCardPreview: function(card) {
+    var NotificationCardView,
+        CardView = require('public-board/views/' + card.type),
+        CardModel = require('core/models/' + card.type);
+
+    NotificationCardView = CardView.extend({
+      tagName: 'div',
+
+      onClick: function() {
+        this.broadcast('router:navigate', this.model.getUrl());
+      }
+    })
+
+    this.card = new NotificationCardView({
+      model: new CardModel(card)
+    });
+
+    this.getElement('card').html(this.card.render().el);
+  },
+
+  onUnplug: function() {
+    if (this.card) {
+      this.card.unplug();
+    }
   }
 });
 
