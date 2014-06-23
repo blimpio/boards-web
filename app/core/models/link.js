db1e230a8da0d870c617a6c93c1b126ab0a700e1
Elving Rodriguez <elving.pr@gmail.com>
2014-06-23 17:12:40 -0400

    Validate url field when creating link cards.

--- app/core/models/link.js
+++ app/core/models/link.js
@@ -7,6 +7,16 @@ module.exports = File.extend({
     });
   },
 
+  validations: _.extend({}, File.prototype.validations, {
+    content: function(content) {
+      if (!content) {
+        return 'Every card requires some type of content.';
+      } else if (!Z.Validations.isUrl(content)) {
+        return 'Please provide a valid url.';
+      }
+    }
+  }),
+
   getExtension: function() {
     return _.last(this.get('content').split('.')).replace(/\/.+|\//, '');
   }
